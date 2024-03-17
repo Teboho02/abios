c27d267311e79c820f349b3f1743dcd6ef30d126
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2024-03-17 21:24:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
-console.log("star");
-//validate the input
+
 const signin = document.getElementById("signin");
 const username = document.getElementById("user");
 const password = document.getElementById("pass");
-
-function validate(username , password){
-
-
-    if(username ==="" || password === ""){
-
-        return 0;
-    }
-
-    return 1;
-    
-    
-        
-}
+const createAccount = document.getElementById("createAcc");
 
 
-signin.addEventLister("click", function(){
 
-    userName = username.textContent;
-    if(validate===1){
-        //authenticate the password and login if correct credentials are provided
-    }
+createAccount.addEventListener('click', () =>{
+    window.location.href = 'signup.html';
+});
 
-    else{
-        console.log("unfilled form somewhere");
-    }
-    
-        
-};
+
